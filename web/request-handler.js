d0378be0778b4063ed335aa4a5e6b26502f4a79f
whitzhu <whitzhu@gmail.com>
2017-03-01 17:41:06 -0800

    return the content of index.html and progress on getting data

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,36 @@
 var path = require('path');
+var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var results = [];
+  if ( req.method === 'POST') {
+    var body = '';
+    req.on('data', function(chunk) {
+      body += chunk;
+    });
+    req.on('end', function() {
+      fs.appendFile(archive.paths.list + '\n', body, function(err) {
+        console.log(err);
+      });
+      console.log('body', body);
+      // results.push(JSON.parse(body));
+    });
+    // console.log('results', results);
+  }
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  
+  fs.readFile(__dirname + '/public/index.html', function(err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.end(data);      
+    }
+  });
+
+
 };
+
